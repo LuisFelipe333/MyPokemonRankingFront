adc2e6e7d7844f690884c400f2224c8966f6d213
luis felipe amador bravo <luigifelipe333@gmail.com>
2026-07-09 23:00:03 -0600

    Se agrega servicio y variables de entorno

---
+++ src/app/core/services/pokemon.service.ts
@@ -0,0 +1,44 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pokemon } from '../models/pokemon';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private apiUrl = `${environment.apiUrl}/pokemon`;
+  
+  
+  private http = inject(HttpClient);
+
+  // OBTENER EL RANKING (GET)
+  getRanking(type?: string, generation?: number): Observable<Pokemon[]> {
+    let params = new HttpParams();
+    
+    if (type) {
+      params = params.set('type', type);
+    }
+    if (generation) {
+      params = params.set('generation', generation.toString());
+    }
+
+    return this.http.get<Pokemon[]>(this.apiUrl, { params });
+  }
+
+  // ELIMINAR DEL RANKING (DELETE)
+  deleteFromRanking(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  addtoRanking(pokemonData: { pokemonApiId: number; name: string }): Observable<Pokemon> {
+    return this.http.post<Pokemon>(this.apiUrl, pokemonData);
+  }
+
+  updatePosition(id: number, newPosition: number): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}/position`, { newPosition });
+  }
+
+}
